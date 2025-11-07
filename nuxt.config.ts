e200be362885145b31fdce2e4d1088ec539a4ed5
戴盛利 <1639499287@qq.com>
2025-11-07 18:16:51 +0800

    添加登录页面和项目元信息配置

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: false }
-})
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: false },
+  app: {
+    head: {
+      title: "Lightning",
+      meta: [
+        {
+          name: "description",
+          content:
+            "Lightning is a fast, modern, and powerful web framework for building web applications.",
+        },
+      ],
+    },
+  }
+});
